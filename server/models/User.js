f1a152615d204a756952d886f2a85a74930a95bb
Jacob Jeffries <jeffrij85@gmail.com>
2023-05-04 20:06:20 -0400

    Actually typed it in the user.js file instead of the index.js file this time

---
+++ server/models/User.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+import { isEmail } from 'validator';
+
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+const Review = require('./Review');
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    validate: [isEmail, 'invalid email']
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  parksVisited: {
+    type: [String]
+  },
+  whishList: {
+    type: [String]
+  },
+  reviews: [Review.schema]
+});
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
