f1a4776d6785433a746095b8771bf6a58cfd232c
Jacob Jeffries <jeffrij85@gmail.com>
2023-05-07 10:03:07 -0400

    Created site scraper

--- server/config/seed.js
+++ server/config/seed.js
@@ -2,14 +2,25 @@ const db = require('./connection');
 const states = require('states-us')
 const abbrev = states.default.map((x) => x.abbreviation);
 const { Park, User } = require('../models');
+let count = 0;
+const allData = []
 
 async function siteData(CODE) {
-  const res = await fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${CODE[0]}&api_key=a87d5fKBWTnIgwEPzboHNuChJvzEYt0hWbIahnCj`)
-  const data = await res.json();
-  console.log(data);
-  const allData = []
-  allData.push(data.data.map((x) => x));
-  console.log(allData);
+  for ( let i = 0; i < CODE.length; i++ ){
+    console.log(`--------------------------------------------------------`);
+    console.log(`--Fetching Park data for ${CODE[i]}.`);
+    const res = await fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${CODE[i]}&api_key=a87d5fKBWTnIgwEPzboHNuChJvzEYt0hWbIahnCj`)
+    const data = await res.json();
+    console.log(`--Total sites found: ${data.total}`)
+    count = parseInt(data.total) + count
+    data.data.map((x) => {
+      console.log(x.fullName);
+      allData.push(x);
+    });
+    console.log(`*Total sites accumulated: ${count}.*`);
+
+  }
+  console.log(allData.length);
   process.exit();
 };
 
