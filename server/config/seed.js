98531c3dedba287dd1e6b36b90fe9583157642e8
Jacob Jeffries <jeffrij85@gmail.com>
2023-05-06 22:08:00 -0400

    Modified seed.js file to include User

--- server/config/seed.js
+++ server/config/seed.js
@@ -1,9 +1,11 @@
 const db = require('./connection');
 const parkData = require('../models/seed/parkData');
-const { Park } = require('../models');
+const { Park, User } = require('../models');
 
 db.once('open', async () => {
+  await User.deleteMany();
   await Park.deleteMany();
+
 
   await Park.insertMany(parkData);
   console.table(parkData);
