dcc6b7cb54888ffd9ef946951cf950b620f20c30
Dgalus <damiangalus7@gmail.com>
2023-05-09 14:06:16 -0400

    added auth utils for client and server side

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,52 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_REVIEW = gql`
+  mutation addReview(
+    $userId: ID!,
+    $body: String!,
+    $score: Int!,
+    $parkCode: String!
+    ) {
+        addReview(userId: $userId, parkCode: $parkCode, body: $body, score: $score,) {
+          userId
+          parkCode
+          body
+          author
+          score
+          createdAt
+          updatedat
+        }
+    }
+`
+
+export const ADD_USER = gql`
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+        token
+        user {
+          _id
+      }
+    }
+  }
+`;
