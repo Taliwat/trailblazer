50994c95e7b4f910c90aa30a8d7c444125a8d51f
scicluna <88948700+scicluna@users.noreply.github.com>
2023-05-08 20:55:25 -0400

    Added state search filter and mapbox logic to match

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
+import { useState } from "react";
 import { HomePage } from "./pages/HomePage.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import ParkPage from "./pages/ParkPage.js";
 import NavBar from "./components/Navigation.js";
 
+
+
 function App() {
+  //eventually shift to useContext component?
+  const [state, setState] = useState('MI')
+
+  function newState(state) {
+    setState(state)
+  }
+
+  console.log(state)
+
   return (
     <BrowserRouter>
-      <NavBar/>
+      <NavBar state={state} newState={newState} />
       <Routes>
-        {/* ////USERCONTEXT
-    ////NAVBAR
-    //////ROUTES
+        {/* {/* ////USERCONTEXT */}
+        {/* //////ROUTES
     ////////ROUTE HOME
     //////////HOMEPAGE (probably mapbox is imported there) */}
-        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/" element={<HomePage state={state} />} />
         {/* {/* ////////ROUTE LOGIN
     //////////LOGIN
     ////////ROUTE SIGNUP
     //////////SIGNUP */}
         {/* ////////ROUTE SPECIFIC PARK */}
-        {/* <Route path="/park/:parkCode" element={<ParkPage />} /> */}
+        <Route path="/park/:parkCode" element={<ParkPage />} />
         {/* //////////SPECIFICPARK
     //////ROUTES
     ////USERCONTEXT */}
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,23 +1,18 @@
 import { Link } from "react-router-dom"
+import StateFilter from "./StateFilter"
 
-
-
-export default function NavBar () {
+export default function NavBar({ state, newState }) {
     return (
         <nav className="w-full h-20 bg-stone-200 flex justify-between shadow-lg">
-            <div className= "w-1/12 h-full flex justify-center items-center"> 
+            <div className="w-1/12 h-full flex justify-center items-center">
                 <Link to={"/"} className="bg-orange-300 h-5/6 w-1/2"></Link>
             </div>
             <div className=" w-1/12 h-full flex justify-center items-center">
-                <input type="text" className="bg-stone-50 w-full h-1/12"/>
+                <StateFilter state={state} newState={newState} />
             </div>
             <div className=" w-1/12 h-full flex justify-center items-center">
                 <Link to={"/login"} >Login</Link>
             </div>
-            
-            
-
-
         </nav>
     )
 }
