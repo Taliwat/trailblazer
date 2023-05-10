2fe06760e412d885d8ccaa82739bd523f7b188d0
Taliwat <117955430+Taliwat@users.noreply.github.com>
2023-05-09 20:01:19 -0400

    reformat contactus page for tailwind

--- client/src/pages/ContactUs.js
+++ client/src/pages/ContactUs.js
@@ -1,10 +1,8 @@
 
-
-
 //create a Contact page that gives information about the team and how to contact us!  be a bit creative here, and be able to have users have a button that can send emails given the provided email in the team member's profile.
 export default function ContactUs () {
     return (
-<div className="aboutus section">
+<div className="contact us section">
 
     <h2 className="text-align-center ">Meet the Team!</h2>
     <div className="row-span-4">
@@ -18,8 +16,7 @@ export default function ContactUs () {
   </div>
   <div class="px-6 pt-4 pb-2">
     <a href="your_link" className=" inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">John's Github</a>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+    <a href="your_link" className=" inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">John's Email</a>
   </div>
 </div>
 <div class="max-w-sm rounded overflow-hidden shadow-lg">
@@ -31,9 +28,8 @@ export default function ContactUs () {
     </p>
   </div>
   <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  <a href="your_link" className=" inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Jacob's Github</a>
+  <a href="your_link" className=" inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Jacob's Email</a>
   </div>
 </div>
 <div class="max-w-sm rounded overflow-hidden shadow-lg">
@@ -45,9 +41,8 @@ export default function ContactUs () {
     </p>
   </div>
   <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  <a href="your_link" className=" inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Damian's Github</a>
+  <a href="your_link" className=" inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Damian's Email</a>
   </div>
 </div>
 <div class="max-w-sm rounded overflow-hidden shadow-lg">
@@ -59,9 +54,8 @@ export default function ContactUs () {
     </p>
   </div>
   <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  <a href="your_link" className=" inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Ryan's Github</a>
+  <a href="your_link" className=" inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Ryan's Email</a>
   </div>
 </div>
     </div>
