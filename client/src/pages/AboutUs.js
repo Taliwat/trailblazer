e39fcde364da5db56aa7ac234be679abf2e5134e
Taliwat <117955430+Taliwat@users.noreply.github.com>
2023-05-09 20:01:15 -0400

    reformat contactus page for tailwind

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -3,6 +3,15 @@
 
 
 export default function aboutUs () {
+    return (
+        <div className="about Us section">
+            <h2 className="text-align-center">About Us</h2>
+            <p className="mb-3 text-lg text-gray-500 md:text-xl dark:text-gray-400"></p>
+            
 
+
+
+        </div>
+    )
 };
 
