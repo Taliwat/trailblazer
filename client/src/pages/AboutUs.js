25f9da1851260e048fd294a3df2db453b8c3b078
Taliwat <117955430+Taliwat@users.noreply.github.com>
2023-05-09 20:49:23 -0400

    added content in aboutus page

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -6,8 +6,9 @@ export default function aboutUs () {
     return (
         <div className="about Us section">
             <h2 className="text-align-center">About Us</h2>
-            <p className="mb-3 text-lg text-gray-500 md:text-xl dark:text-gray-400"></p>
-            
+            <p className="mb-3 text-lg text-gray-500 md:text-xl dark:text-gray-400">Welcome to Trailblazer, a new website that will help you locate and research national parks in your area as well as what activities you can do there!  Feel free to reach out to us through our Contact Us page with any questions, and we hope you enjoy it!</p>
+            <p className="text-gray-500 dark:text-gray-400"></p>
+
 
 
 
