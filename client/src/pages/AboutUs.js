36193e8a6212010672a5129e4a5eb8f13de5e558
Taliwat <117955430+Taliwat@users.noreply.github.com>
2023-05-11 18:21:48 -0400

    ficed links in about and contact and added content

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -6,7 +6,8 @@ export default function aboutUs () {
     return (
         <div className="about Us section">
             <h2 className="text-align-center">About Us</h2>
-            <p className="mb-3 text-lg text-gray-500 md:text-xl dark:text-gray-400"></p>
+            <p className="mb-3 text-lg text-gray-500 md:text-xl dark:text-gray-400">Welcome to Trailblazer! Our goal is to help you be able to navigate the site easily and find some really useful activities and information for your next trip or day out with your family.  This site was created with the goal in mind that you can find and lookup any information on national parks and find out lots of information about them, maybe even some things you don't know.</p>
+            <p className="mb-3 text-lg text-gray-500 md:text-lg dark:text-gray-400">Please enjoy the site and contact us at any time if you have questions or features you would like to see.  We hope you find what you're looking for!</p>
             
 
 
