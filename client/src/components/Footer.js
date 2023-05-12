4da14decef49de5bba1f1e83168a25bda542f51c
Taliwat <117955430+Taliwat@users.noreply.github.com>
2023-05-11 21:05:06 -0400

    css editing

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 //creates a new footer that has links to go to the About Us and Contact Us pages
 export default function Footer () {
     return (
-        <footer className="w-full h-12 bg-stone-200 flex justify-between shadow-lg border-solid border-2 rounded-lg">
+        <footer className="w-full h-12 bg-gray-300 flex justify-between shadow-lg border-solid border-2 rounded-lg fixed inset-x-0 bottom-0">
             <div className= "w-1/12 h-full flex justify-center items-center font-mono font-semibold"> 
                 <Link to={"/about"} >About Us</Link>
             </div>
